25bbc27067a511ddc9a6ee3bec0a9f07148862cb
Lutti Davide <luttidavide004@gmail.com>
2024-05-31 11:26:14 +0200

    aggiunta documentazione swagger

--- src/main/java/com/example/esercizioSwagger/NameController.java
+++ src/main/java/com/example/esercizioSwagger/NameController.java
@@ -1,18 +1,25 @@
 package com.example.esercizioSwagger;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.*;
 
+@Api(tags = "Name Controller")
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/name")
 public class NameController {
 
-    @GetMapping("/name")
-    public String getName(@RequestParam String name) {
+    @ApiOperation(value = "Get name", notes = "Restituisce il nome fornito come parametro")
+    @GetMapping
+    public String getName(@ApiParam(value = "Nome da restituire", required = true) @RequestParam String name) {
         return name;
     }
 
-    @PostMapping("/name/reverse")
-    public String reverseName(@RequestBody String name) {
+    @ApiOperation(value = "Get reversed name", notes = "Restituisce il nome fornito come parametro, ma al contrario")
+    @PostMapping("/reverse")
+    public String reverseName(@ApiParam(value = "Nome da restituire al contrario", required = true) @RequestParam String name) {
         return new StringBuilder(name).reverse().toString();
     }
+
 }
